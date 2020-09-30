e7df257c10c6f7bc2b7b429ba85622d9d02a5b78
Parag-Chaudhary <70112406+Parag-Chaudhary@users.noreply.github.com>
2020-09-30 12:28:44 +0530

    Update Mask_Detector_using_model.py

--- Mask_Detector_using_model.py
+++ Mask_Detector_using_model.py
@@ -2,7 +2,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import cv2
-import imutils
 import numpy as np
 
 model = load_model('Mask_Detector.model')
@@ -13,12 +12,8 @@ cap = cv2.VideoCapture(0)
 labels_dict={0:'MASK',1:'NO MASK'}
 color_dict={0:(0,255,0),1:(0,0,255)}
 
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# out = cv2.VideoWriter('output.avi', fourcc, 24.0, (640, 480))
-
 while True:
     ret, frame = cap.read()
-    #frame = imutils.resize(frame, width=1024)
     color = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     faces = face_cascade.detectMultiScale(color,1.3,5)
 
@@ -39,8 +34,6 @@ while True:
         cv2.rectangle(frame,(x,y-40),(x+w,y),color_dict[labels],-1)
         cv2.putText(frame,labels_dict[labels],(x,y-10), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
         cv2.putText(frame,str(percentage),(x+130,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),2)
-
-        # out.write(frame)
 
     cv2.imshow('CAMERA', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
