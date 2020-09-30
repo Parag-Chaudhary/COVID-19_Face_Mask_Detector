62627a2bd7c3fefb001b8c841cc1912e31a5cd83
Parag-Chaudhary <70112406+Parag-Chaudhary@users.noreply.github.com>
2020-09-30 13:19:33 +0530

    Update Train_Mask_Detector.py

--- Train_Mask_Detector.py
+++ Train_Mask_Detector.py
@@ -1,24 +1,21 @@
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-#
-from tensorflow.keras import layers
-from tensorflow.keras.layers import Input, Dense, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, Dropout
-from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, GlobalAveragePooling2D, GlobalMaxPooling2D
+
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout
+from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
-#
+
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
-#
+
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.utils import normalize
-#
+
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-#
-from imutils import paths
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
